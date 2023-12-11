1a8a6c09a41434d9d35a51145f133ebb4be9d4ba
YoussefZaki119 <youssefzaki2002@gmail.com@DESKTOP-LHVS2PK>
2023-12-11 21:24:31 +0200

    std

--- src/Stadiums/stadiums.controller.ts
+++ src/Stadiums/stadiums.controller.ts
@@ -1,27 +1,26 @@
 
 
-import { Controller, Get, Query } from '@nestjs/common'
-import { StadiumsService } from './stadiums.model'
+import { Controller, Body, Get, Post, Query } from '@nestjs/common'
+import { StadiumsService } from './stadiums.service'
 import { Stadium } from './stadiums.model';
 import { StadiumDto } from './stadium.dto';
 @Controller('stadiums')
-export class StadiumsController
-{
-    constructor(private readonly stadiumsService: StadiumsService){}
+export class StadiumsController {
+    constructor(private readonly stadiumsService: StadiumsService) { }
 
-@Get()
-findall(@Query('stadium') stadium: string
-    ): Promise<Stadium[]>{
-        return this.stadiumsService.loadAllstadiums(stadium);
+    @Get()
+    findall(@Query('stadium') stadium: string
+    ): Promise<Stadium[]> {
+        return this.stadiumsService.loadAllStadiums(stadium);
 
     }
 
 
 
 
-@Post() // Use the HTTP POST method for inserting data
-async create(@Body() stadiumDto: StadiumDto): Promise < Stadium > {
-    return this.teamsService.createStadium(stadiumDto);
-}
+    @Post() // Use the HTTP POST method for inserting data
+    async create(@Body() stadiumDto: StadiumDto): Promise<Stadium> {
+        return this.stadiumsService.createStadium(stadiumDto);
+    }
 }
 
