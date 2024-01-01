5108907daaf4fbdebf4e7e621efa0675002b8e33
jomana1 <jomana.abdelnaim02@eng-st.cu.edu.eg>
2024-01-01 20:45:12 +0200

    bug fixes

--- src/Referees/referees.service.ts
+++ src/Referees/referees.service.ts
@@ -1,55 +1,34 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Op } from 'sequelize';
 import { Referee } from './referees.model';
+
 @Injectable()
-export class RefereesService{
-    
+export class RefereesService {
     constructor(
         @InjectModel(Referee)
         private refereeModel: typeof Referee
-    ){}
-    async loadAllReferees(
-        title?: string
-    ): Promise<Referee[]> {
+    ) {}
 
-        let where = {};
-
-        // if (title) {
-        //     where = {
-        //         name: {
-        //             [Op.iLike]: `%${title}%`
-        //         }
-        //     }
-        // }
-        const referees = await this.refereeModel.findAll({ where });
+    async loadAllReferees(): Promise<Referee[]> {
+        const referees = await this.refereeModel.findAll();
         return referees;
     }
-    async loadAllMainReferees(title?: string): Promise<Referee[]> {
-        // let where = {};
 
-        // if (title) {
-        //     where = {
-        //         name: { [Op.iLike]: `%${title}%` }
-        //     };
-        // }
-
-        const referee = await this.refereeModel.findAll();
-        return referee;
+    async findRefereeById(id: number): Promise<Referee> {
+        const referees = await this.refereeModel.findOne({ where: { id } });
+        return referees;
     }
-    async loadAssisinatReferees(
-        title?: string
-    ): Promise<Referee[]> {
-        let where = {};
 
-        if (title) {
-            where = {
-                name: { [Op.iLike]: `%${title}%` }
-            };
-        }
 
-        const referee = await this.refereeModel.findAll({ where: { role: 'Assistant Referee' } });
-        return referee;
+
+    async loadAllMainReferees(): Promise<Referee[]> {
+        const referees = await this.refereeModel.findAll({ where: { role: 'Head Referee' } });
+        return referees;
+    }
+
+    async loadAssistantReferees(): Promise<Referee[]> {
+        const referees = await this.refereeModel.findAll({ where: { role: 'Assistant Referee' } });
+        return referees;
     }
 }
